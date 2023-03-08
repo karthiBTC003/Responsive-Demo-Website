ad8437e56735979efb5ab3f52a76146e676394d0
karthiBTC003 <hardikkarthi901@gmail.com>
2023-03-08 18:45:08 +0530

    some change

--- frist.js
+++ frist.js
@@ -341,7 +341,19 @@ myForm.addEventListener("submit", function(event) {
 });
 
 
-$('.single-item').slick();
+$('.single-item').slick(
+  {
+    dots: true,
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  }
+);
 
 
 
